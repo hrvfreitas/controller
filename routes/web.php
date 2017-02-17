72c5161375b1f0196bf601731867c2e71364b61a
php 12 <php.12@metralha.cec.ime.usp.br>
2017-02-16 22:04:51 -0200

    segundo commit

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,9 @@
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+
+Route::get('/editoras', 'EditoraController@index');
+Route::get('/editoras/cria', 'EditoraController@cria');
+Route::post('editoras', 'EditoraController@armazena');
